3db71b3ea04c660373446aca6962e11739167620
Jack-123-tech <71794471+Jack-123-tech@users.noreply.github.com>
2020-09-30 18:02:34 -0400

    Average program before loop and print

--- average.py
+++ average.py
@@ -1,2 +1,21 @@
-numberofScores=input("Please enter the number of scoures you want average:")
-print(numberofScores)
+#Initialaize all variables to 0
+numberOfScores=0
+score=0
+total=0
+scoreCount=0
+average=0.0
+# Accept the number of scores to average
+numberOfScores=input("Please enter the number of scoures you want average:")
+#Add a loop to make this code repeat until scoreCount = numberOfScores
+#scoreCount less than numberOfScores
+while
+score=int(input("Please enter a score:"))
+total= total + score
+scoreCount=scoreCount + 1
+
+
+
+
+
+average=total / numberOfScores
+print(average)
