0138336b680674ebe9aa609c612faa2c314e3df2
Jack-123-tech <71794471+Jack-123-tech@users.noreply.github.com>
2020-11-24 12:22:35 -0500

    Update PythonQueue.py

--- PythonQueue.py
+++ PythonQueue.py
@@ -1,10 +1,10 @@
-names=["jack","Ben","tyler","Sammy","Justin","Brain","Nick","Ryan","Danny","ashley"]
-
+Names=["jack","Ben","tyler","Sammy","Justin","Brain","Nick","Ryan","Danny","ashley"]
+name=[]
 
 #Hints names=[] name.append(acceptedName)
 for i in range(0, 10):
     acceptedName = str(input("Please enter a name: "))
-    names.append(acceptedName)
+    name.append(Names)
 #len(names) print(names.pop(0))
 # two for loops must be in the code
 #sim a queue at a bank
