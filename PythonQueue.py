dfd87a19c4b0642248165516a2d8baa49d2e328d
Jack-123-tech <71794471+Jack-123-tech@users.noreply.github.com>
2020-11-26 23:21:40 -0500

    Add files via upload

---
+++ PythonQueue.py
@@ -0,0 +1,14 @@
+Names=["jack","Ben","tyler","Sammy","Justin","Brain","Nick","Ryan","Danny","ashley"]
+name=[]
+
+#Hints names=[] name.append(acceptedName)
+for i in range(0, 10):
+    acceptedName = str(input("Please enter a name: "))
+    name.append(Names)
+#len(names) print(names.pop(0))
+# two for loops must be in the code
+#sim a queue at a bank
+
+
+for j in range(0, len(names)):
+    print(names.pop(0))
